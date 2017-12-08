154a985a0f84c23f3f2253cd76fdfa3caf5001bb
desu314 <1162903270@qq.com>
2017-12-08 10:53:07 +0800

    '新增电脑端自动收货商家分账'

--- api/okgoods.php
+++ api/okgoods.php
@@ -20,7 +20,6 @@ require('../includes/init.php');
 // 自动确认收货
 $okg = $GLOBALS['db']->getAll("select order_id, add_time from " . $GLOBALS['ecs']->table('order_info') . " where shipping_status = 1 and order_status in(1,5,6)");
 $okgoods_time = $GLOBALS['db']->getOne("select value from " . $GLOBALS['ecs']->table('shop_config') . " where code='okgoods_time'");
-
 foreach($okg as $okg_id)
 {
 	$okg_time = $okgoods_time - (local_date('d',gmtime()) - local_date('d',$okg_id['add_time']));
@@ -30,6 +29,11 @@ foreach($okg as $okg_id)
 	if ($okg_time <= 0 && $is_back_now == 0)
 	{
 		$db->query("update " . $ecs->table('order_info') . " set shipping_status = 2, shipping_time_end = " . gmtime() . "  where order_id = " . $okg_id['order_id']);
+
+        //todo 给商家打款
+        include_once(ROOT_PATH . 'includes/lib_order.php');
+        get_pingtai_rebate_from_supplier($okg_id['order_id']);
+        $GLOBALS['db']->query("UPDATE " . $GLOBALS['ecs']->table('order_info') . " SET rebate_ispay = 2 WHERE order_id = ".$okg_id['order_id']);
 	}
 }
 
