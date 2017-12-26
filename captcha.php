f5fd80766986742e470db8a8bccd7091609bd1c9
MichaelLiMing <326040115@qq.com>
2017-12-26 09:57:39 +0800

    验证码

--- captcha.php
+++ captcha.php
@@ -18,9 +18,10 @@ define('INIT_NO_SMARTY', true);
 
 require(dirname(__FILE__) . '/includes/init.php');
 require(ROOT_PATH . 'includes/cls_captcha.php');
-echo 1;
-die;
+
 $img = new captcha(ROOT_PATH . 'data/captcha/', $_CFG['captcha_width'], $_CFG['captcha_height']);
+echo 2;
+die;
 @ob_end_clean(); //清除之前出现的多余输入
 if (isset($_REQUEST['is_login']))
 {
