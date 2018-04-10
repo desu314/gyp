7dd0b45ef5c6a272fd8f29d843852f4f4fd50dce
MichaelLiMing <326040115@qq.com>
2018-04-10 21:51:47 +0800

    1

--- apply_index.php
+++ apply_index.php
@@ -24,9 +24,9 @@ if ((DEBUG_MODE & 2) != 2)
 }
 
 if (!$smarty->is_cached($templates, $cache_id))
-{ 
-	
-	
+{
+
+
     /* 模板赋值 */
     assign_template();
     $position = assign_ur_here(0, $GLOBALS['_LANG']['apply_index']);
@@ -35,24 +35,24 @@ if (!$smarty->is_cached($templates, $cache_id))
     $smarty->assign('helps',            get_shop_help()); // 网店帮助
     $smarty->assign('all',   	$cats['all']);
     $smarty->assign('tuijian',       $tuijian);
-    
+
     $smarty->assign('logopath',		'/'.DATA_DIR.'/supplier/logo/');
     $smarty->assign('shops_list',   $shop_list['shops']);
     $smarty->assign('filter',       $shop_list['filter']);
     $smarty->assign('record_count', $shop_list['record_count']);
     $smarty->assign('page_count',   $shop_list['page_count']);
-    
+
     $page = (isset($_REQUEST['page'])) ? intval($_REQUEST['page']) : 1;
-    
+
     $start_array = range(1,$page);
     $end_array   = range($page,$shop_list['page_count']);
     if($page-5>0){
-    	$smarty->assign('start',$page-3);
-    	$start_array = range($page,$page-2);
+        $smarty->assign('start',$page-3);
+        $start_array = range($page,$page-2);
     }
     if($shop_list['page_count'] - $page > 5){
-    	$smarty->assign('end',$page+3);
-    	$end_array   = range($page,$page+2);
+        $smarty->assign('end',$page+3);
+        $end_array   = range($page,$page+2);
     }
     $page_array  = array_merge($start_array,$end_array);
     sort($page_array);
@@ -60,8 +60,8 @@ if (!$smarty->is_cached($templates, $cache_id))
 }
 if ($action == 'store_joinin')
 {
-    
-	
+
+
 }
 //判断 弹框登陆 验证码是否显示
 $captcha = intval($_CFG['captcha']);
@@ -70,6 +70,6 @@ if(($captcha & CAPTCHA_LOGIN) && (! ($captcha & CAPTCHA_LOGIN_FAIL) || (($captch
     $GLOBALS['smarty']->assign('enabled_captcha', 1);
     $GLOBALS['smarty']->assign('rand', mt_rand());
 }
- $smarty->display('apply_index.dwt');
+$smarty->display('apply_index.dwt');
 
 ?>
