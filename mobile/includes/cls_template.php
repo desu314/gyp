26a479b374c2837bfa96dec6c9133ea32300c64f
MichaelLiMing <326040115@qq.com>
2018-04-24 20:07:39 +0800

    手机端打开店铺报错

--- mobile/includes/cls_template.php
+++ mobile/includes/cls_template.php
@@ -1051,9 +1051,9 @@ class cls_template
         if ($file_type == '.dwt')
         {
             /* 将模板中所有library替换为链接 */
-            $pattern     = '/<!--\s#BeginLibraryItem\s\"\/(.*?)\"\s-->.*?<!--\s#EndLibraryItem\s-->/se';
-            $replacement = "'{include file='.strtolower('\\1'). '}'";
-            $source      = preg_replace($pattern, $replacement, $source);
+            $pattern     = '/<!--\s#BeginLibraryItem\s\"\/(.*?)\"\s-->.*?<!--\s#EndLibraryItem\s-->/s';
+            //$replacement = "'{include file='.strtolower('\\1'). '}'";
+            $source      = preg_replace($pattern, function ($r){ return "'{include file='.strtolower($r[1]). '}'";}, $source);
 
             /* 检查有无动态库文件，如果有为其赋值 */
             $dyna_libs = get_dyna_libs($GLOBALS['_CFG']['template'], $this->_current_file);
